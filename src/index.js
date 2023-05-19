3cb4a1f39c76a89c5fea989e567e95fae57edaf3
Andy-Melvin <andymelvin56@gmail.com>
2023-05-20 00:28:56 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
+
 import '@fontsource/poppins';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
